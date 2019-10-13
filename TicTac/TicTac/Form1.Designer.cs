5683ff6b867f4e0e72d6e69b3f61cbaa432b5d14
DKrivenko-hub <56490485+DKrivenko-hub@users.noreply.github.com>
2019-10-13 14:46:31 +0300

    TicTacToyGame

---
+++ TicTac/TicTac/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_Game = new System.Windows.Forms.Button();
+            this.WinsX_label = new System.Windows.Forms.Label();
+            this.WinsO_label = new System.Windows.Forms.Label();
+            this.label_scoreX = new System.Windows.Forms.Label();
+            this.label_scoreO = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start_Game
+            // 
+            this.Start_Game.Location = new System.Drawing.Point(350, 42);
+            this.Start_Game.Name = "Start_Game";
+            this.Start_Game.Size = new System.Drawing.Size(75, 23);
+            this.Start_Game.TabIndex = 0;
+            this.Start_Game.Text = "Start_Game";
+            this.Start_Game.UseVisualStyleBackColor = true;
+            this.Start_Game.Click += new System.EventHandler(this.Start_Game_Click);
+            // 
+            // WinsX_label
+            // 
+            this.WinsX_label.AutoSize = true;
+            this.WinsX_label.Location = new System.Drawing.Point(324, 129);
+            this.WinsX_label.Name = "WinsX_label";
+            this.WinsX_label.Size = new System.Drawing.Size(49, 13);
+            this.WinsX_label.TabIndex = 1;
+            this.WinsX_label.Text = "Побед Х";
+            // 
+            // WinsO_label
+            // 
+            this.WinsO_label.AutoSize = true;
+            this.WinsO_label.Location = new System.Drawing.Point(323, 158);
+            this.WinsO_label.Name = "WinsO_label";
+            this.WinsO_label.Size = new System.Drawing.Size(50, 13);
+            this.WinsO_label.TabIndex = 2;
+            this.WinsO_label.Text = "Побед О";
+            // 
+            // label_scoreX
+            // 
+            this.label_scoreX.AutoSize = true;
+            this.label_scoreX.Location = new System.Drawing.Point(424, 129);
+            this.label_scoreX.Name = "label_scoreX";
+            this.label_scoreX.Size = new System.Drawing.Size(13, 13);
+            this.label_scoreX.TabIndex = 3;
+            this.label_scoreX.Text = "0";
+            // 
+            // label_scoreO
+            // 
+            this.label_scoreO.AutoSize = true;
+            this.label_scoreO.Location = new System.Drawing.Point(424, 158);
+            this.label_scoreO.Name = "label_scoreO";
+            this.label_scoreO.Size = new System.Drawing.Size(13, 13);
+            this.label_scoreO.TabIndex = 4;
+            this.label_scoreO.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 212);
+            this.Controls.Add(this.label_scoreO);
+            this.Controls.Add(this.label_scoreX);
+            this.Controls.Add(this.WinsO_label);
+            this.Controls.Add(this.WinsX_label);
+            this.Controls.Add(this.Start_Game);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Game;
+        private System.Windows.Forms.Label WinsX_label;
+        private System.Windows.Forms.Label WinsO_label;
+        private System.Windows.Forms.Label label_scoreX;
+        private System.Windows.Forms.Label label_scoreO;
+    }
+}
+
---
+++ TicTac/TicTac/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        Button[,] btn;//Array of buttons
+
+        int[,] GameField;//Array with 1 and -1. 1-X, -1-O
+
+        int N = 3;//Size Game Field
+        int number_turn = 0; //Turn sum
+        string Turn = "X";//priority of turn
+
+        int ScoreX = 0;//number of Player_X wins
+        int ScoreO = 0;//number of Player_O wins
+
+        Label Query;//Shows sequence of move
+
+        public Form1()//Constructor Main Window
+        {
+            InitializeComponent();
+
+
+            //Button responsible for Start New Game 
+            { 
+                Start_Game.Text = "Новая Игра";
+                Start_Game.Size = new Size(75, 50);
+            }
+
+            //Transform Form Settings
+            {
+                MaximumSize = new Size(500, 250);
+                MinimumSize = new Size(500, 250);
+                Name = "TicTacToe";
+                Text = "TicTacToe";
+            }
+
+            GameField = new int[N, N];//Create Array
+
+            {
+                Query = new Label();//Create instance of Label
+                                    //Transform Settings
+                Query.Text = "Ходят " + Turn;
+                Controls.Add(Query);//Add component to the Form
+                Query.Location = new Point(50, 150);//Transform Location
+            }
+
+            //Create, Add and transform components "Button"          
+            {
+                btn = new Button[N, N];
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        btn[i, j] = new Button();
+                        btn[i, j].Name = i.ToString() + j.ToString();
+                        btn[i, j].Size = new Size(50, 50);
+                        btn[i, j].Location = new Point(50 * i, 50 * j);
+                        btn[i, j].BringToFront();
+                        btn[i, j].Click += Button_Click;//Click EventHandler
+                        Controls.Add(btn[i, j]);
+                    }
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)//Event "Click Button" in Array of buttons
+        {
+            Button btn = (Button)sender;//Downcast
+            number_turn++;
+            int X = btn.Location.X / btn.Width;//Coordinate X
+            int Y = btn.Location.Y / btn.Height;//Coordinate Y
+
+            if (Turn == "X")
+            {
+                btn.BackgroundImage = Properties.Resources.Cross;//Add picture from resources to Button
+                btn.BackgroundImageLayout = ImageLayout.Zoom;//centering picture
+                // btn.BackColor = Color.Red;
+                btn.Enabled = false;
+                Turn = "O";//Change sequence of move
+                GameField[X, Y] = 1;
+                Query.Text = "Ходят " + Turn;//Write whoose turn
+            }
+            else
+            {
+                btn.BackgroundImage = Properties.Resources.Zero;
+                btn.BackgroundImageLayout = ImageLayout.Zoom;
+                //btn.BackColor = Color.Black;
+                Turn = "X";
+                btn.Enabled = false;
+                GameField[X, Y] = -1;
+                Query.Text = "Ходят " + Turn;
+            }
+
+           // Checking
+            if (UnitedCheck() == "X")
+            {
+                ScoreX++;
+                label_scoreX.Text = ScoreX.ToString();
+                ClearField();
+            }
+            else if (UnitedCheck() == "O")
+            {
+                ScoreO++;
+                label_scoreO.Text = ScoreO.ToString();
+                ClearField();
+            }
+            else if (number_turn == N * N)//If all buttons are pressed
+            {
+                MessageBox.Show("Ничья", "Ничья");
+                ClearField();
+            }
+
+        }
+        string UnitedCheck()
+        {
+            if (CheckHoriz() == "X" ||
+                CheckVert() == "X" ||
+                CheckDiag() == "X")
+            {
+                return "X";
+            }
+            if (CheckHoriz() == "O" ||
+                CheckVert() == "O" ||
+                CheckDiag() == "O")
+            {
+                return "O";
+            }
+            return "0";
+        }
+        string CheckHoriz()
+        {
+            if ((GameField[0, 0] == 1 && GameField[0, 1] == 1 && GameField[0, 2] == 1) ||
+                (GameField[1, 0] == 1 && GameField[1, 1] == 1 && GameField[1, 2] == 1) ||
+                (GameField[2, 0] == 1 && GameField[2, 1] == 1 && GameField[2, 2] == 1))
+            {
+                return "X";
+            }
+            if ((GameField[0, 0] == -1 && GameField[0, 1] == -1 && GameField[0, 2] == -1) ||
+                (GameField[1, 0] == -1 && GameField[1, 1] == -1 && GameField[1, 2] == -1) ||
+                (GameField[2, 0] == -1 && GameField[2, 1] == -1 && GameField[2, 2] == -1))
+            {
+
+                return "O";
+            }
+            return "0";
+        }
+        string CheckVert()
+        {
+            if ((GameField[0, 0] == 1 && GameField[1, 0] == 1 && GameField[2, 0] == 1) ||
+                (GameField[0, 1] == 1 && GameField[1, 1] == 1 && GameField[2, 1] == 1) ||
+                (GameField[0, 2] == 1 && GameField[1, 2] == 1 && GameField[2, 2] == 1))
+            {
+                return "X";
+            }
+            if ((GameField[0, 0] == -1 && GameField[1, 0] == -1 && GameField[2, 0] == -1) ||
+                (GameField[0, 1] == -1 && GameField[1, 1] == -1 && GameField[2, 1] == -1) ||
+                (GameField[0, 2] == -1 && GameField[1, 2] == -1 && GameField[2, 2] == -1))
+            {
+                return "O";
+            }
+            return "0";
+        }
+        string CheckDiag()
+        {
+            if ((GameField[0, 0] == 1 && GameField[1, 1] == 1 && GameField[2, 2] == 1) ||
+                (GameField[0, 2] == 1 && GameField[1, 1] == 1 && GameField[2, 0] == 1))
+            {
+                return "X";
+            }
+            if ((GameField[0, 0] == -1 && GameField[1, 1] == -1 && GameField[2, 2] == -1) ||
+                (GameField[0, 2] == -1 && GameField[1, 1] == -1 && GameField[2, 0] == -1))
+            {
+                return "O";
+            }
+            return "0";
+        }
+
+        void ClearField()//Clear Game Field. Return all System settings to buttons and Clear Array of values
+        {
+            number_turn = 0;
+            Thread.Sleep(1000);//Stopping programm on 1 sec
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    btn[i, j].Enabled = true;
+                    btn[i, j].BackgroundImage = null;
+                    // btn[i, j].BackColor = Color.Empty;
+                    GameField[i, j] = 0;
+                }
+            }
+        }
+
+        private void Start_Game_Click(object sender, EventArgs e)//Event pressing the button "New Game" 
+        {
+            ClearField();//Run cleaning method
+            
+            //Clear score of players
+            ScoreO = 0;
+            ScoreX = 0;
+
+            label_scoreO.Text = "0";
+            label_scoreX.Text = "0";
+        }
+    }
+}
